8d24e0d3025d1c4b5d668f2cd1e74ef5c9675a54
Raghav Kaushik <raghav.kaushik611@gmail.com>
2020-02-18 20:42:58 +0530

    socket connection

--- client/src/boot/socket.js
+++ client/src/boot/socket.js
@@ -1,5 +1,7 @@
+import Vue from 'vue';
 import socketIOClient from 'socket.io-client'
 
 const socket = socketIOClient('http://localhost:3000')
 
-export default socket
+Vue.prototype.$socket = socket    // this.$socket
+// export default socket
