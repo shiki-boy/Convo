680ab00a048b950b8d329c4921c5cccd96462bdf
Raghav Kaushik <raghav.kaushik611@gmail.com>
2020-02-18 20:45:47 +0530

    axios instance

--- client/src/boot/axios.js
+++ client/src/boot/axios.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-Vue.prototype.$axios = axios
+// We create our own axios instance and set a custom base URL.
+// Note that if we wouldn't set any config here we do not need
+// a named export, as we could just `import axios from 'axios'`
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:3000'
+})
+
+Vue.prototype.$axios = axiosInstance  // this.$axios
--- client/src/boot/socket.js
+++ client/src/boot/socket.js
@@ -4,4 +4,3 @@ import socketIOClient from 'socket.io-client'
 const socket = socketIOClient('http://localhost:3000')
 
 Vue.prototype.$socket = socket    // this.$socket
-// export default socket
